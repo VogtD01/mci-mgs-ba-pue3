29e219b02ae5751f76cf3078c867b2d0ad61bfeb
undjacob <34163347+undjacob@users.noreply.github.com>
2022-10-31 08:48:59 +0100

    Add files via upload

---
+++ examples/GPIOs/Digital_Output/Morse_Code/main.py
@@ -0,0 +1,11 @@
+from machine import Pin
+import libs.morsecode as morsecode
+import time
+
+
+# Set GPIO2 to output
+led_pin = Pin(2, Pin.OUT)
+
+while 1:
+  print("Test and check")
+  morsecode.send("SOS")
