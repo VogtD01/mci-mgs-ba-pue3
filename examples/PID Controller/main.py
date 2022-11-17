ca1dd99eb070e50a49f5a9bb5858c0ab6e55f947
Ephraim Westenberger <ephraim.westenberger@gmail.com>
2022-11-17 15:15:12 +0100

    PID controller update (WIP)

--- examples/PID Controller/main.py
+++ examples/PID Controller/main.py
@@ -19,25 +19,43 @@ Kd = 100
 
 # Inputs zu den Zeitpunkten t0, t-1 und t-2
 inpt = [0,0,0]
-
+inpt[0] = 1
 # Outputs zu den Zeitpunkten t0 und t-1
 output = [0,0]
+
+setpoint = 50
+
 
 def mapping(val_in, min_in, min_out, max_in, max_out):
     return min_out + (max_out - min_out) * (val_in - min_in) / (max_in - min_in)
 
 def ISR(t):
-    inpt[0] = 1
+
+    raw_input = adc0.read_u16()
+    measured = mapping(raw_input, 0, 0, 65535, 100)
+
+    print(f'raw measured: {raw_input}, mapped measured: {measured}')
+
+    inpt[0] = setpoint - measured
+
     output[0] = output[1]+inpt[0]*(Kp+Kd+Ki)-inpt[1]*(2*Kd+Kp)+inpt[2]*Kd
+
     print(f'Input: {inpt[0]} Output: {output[0]}')
+
+
+    led_duty = int(mapping(output[0], 0, 0, 100, 1023))
+
+    print(f'dc: {led_duty}')
+    pwm0.duty(led_duty)
 
     output[1] = output[0]
     inpt[2] = inpt[1]
     inpt[1] = inpt[0]
 
 
+
 tim0 = Timer(1)
-tim0.init(period=1000, mode=Timer.PERIODIC, callback=ISR)
+tim0.init(period=200, mode=Timer.PERIODIC, callback=ISR)
 
 if __name__ == "__main__":
     while True:
@@ -45,6 +63,5 @@ if __name__ == "__main__":
         # Um die ISR Funktion des Timers bei Bedarf zu unterbrechen,
         # überprüft das Programm ob Pin 34 einen HIGH Zustand hat.
         # Ist der Zustand LOW, wird der Timer deinitialisiert.
-        
         if not start_pin.value():
             tim0.deinit()
