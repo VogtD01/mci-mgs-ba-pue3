2321e4480c4ca59b32a80541b4b0e9addff4510e
Ephraim Westenberger <76460220+futuresalad@users.noreply.github.com>
2023-09-26 13:46:59 +0200

    Update main.py

--- examples/GPIOs/Digital_Input/Counting_Pushes/main.py
+++ examples/GPIOs/Digital_Input/Counting_Pushes/main.py
@@ -4,15 +4,21 @@ from machine import Pin
 # Set GPIO2 to output
 led_pin = Pin(2, Pin.OUT)
 switch_pin1 = Pin(4,mode=Pin.IN,pull=Pin.PULL_DOWN)
+
 counter=0
 
 print('Congratulation, the setup works')
-while 1:
-  if switch_pin1.value():
-    counter=counter+1
-    # time.sleep(0.2) #... Sleep Deboucing
-    print(counter)
-    led_pin.value(switch_pin1.value())
-  
+
+while True:
+
+    if switch_pin1.value():
+
+        counter+=1
+
+        #... Sleep Deboucing
+        time.sleep(0.2) 
+        
+        print(counter)
+        led_pin.value(switch_pin1.value())
 
   # Sleep Debouncing has the major problem of "wasting" time, the MCU cannot do anything else but sleeping for 0.2 seconds
